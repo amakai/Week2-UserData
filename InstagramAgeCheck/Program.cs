09ca811adb2fc4bbce8f4625dd249d6e9f9c30eb
Amanda <amanda.kaijanen@gmail.com>
2021-09-13 18:31:06 +0300

    Control Flow

---
+++ InstagramAgeCheck/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace InstagramAgeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {Console.WriteLine("Palun, sisesta oma vanus:");
+           
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            if(userAge > 13)
+            {
+                Console.WriteLine("Oled piisavalt vana, et Intagrami kontot luua.");
+
+            }
+            else if(userAge < 13)
+            {
+                Console.WriteLine("Oled liiga noor, et Instagrami kontot luua.");
+
+            }
+             else
+            {
+                Console.WriteLine("Oledki 13,nüüd saad endale Instagrami kontot luua."); 
+            }
+            Console.WriteLine("Kena päeva!");
+
+        }
+    }
+}
